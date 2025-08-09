53fe47e5e2c4e383eb8788561ae3cfabcc760a50
Exxotelis <telis0kaps@gmail.com>
2025-08-10 00:34:41 +0100

    alloy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,7 +10,7 @@ DEBUG = True  # Set to False in production
 ALLOWED_HOSTS = [
     "exxotelis.com",
     "www.exxotelis.com",
-    "https://itelis-dev-production.up.railway.app",   # π.χ. itelis-dev-production.up.railway.app
+    "itelis-dev-production.up.railway.app",   # π.χ. itelis-dev-production.up.railway.app
 ]
 
 CSRF_TRUSTED_ORIGINS = [
