70736ba1f2d18f0adf4220a07a7b43124d935896
Exxotelis <telis0kaps@gmail.com>
2025-08-10 00:55:57 +0100

    source

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    # Root & SPA fallback
     path("", TemplateView.as_view(template_name="index.html")),
-    re_path(r"^(?!api/).*$", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?!api/)(?!static/)(?!media/).*$", TemplateView.as_view(template_name="index.html")),
 ]
